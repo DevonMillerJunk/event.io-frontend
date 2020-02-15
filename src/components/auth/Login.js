735d76c14b57c55b8a6014e6f249ff136e76ff83
Kabishan <suvendrk@mcmaster.ca>
2020-02-15 18:18:33 -0500

    Cameron reformatted the login page.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import radar from './radar1.png';
 
@@ -18,60 +19,82 @@ const Login = () =>{
     }
 
     return (
-        <div className='card card-lg'>
-          <h1 className='mac-header-nbm'>Event</h1>
-          <h1 style={{ textAlign: 'center' }}>io</h1>
-          <div style={{ width: '30%', margin: '0 auto' }}>
-            <img src={radar} alt="Logo" class="center" />
+        <div className='card card-nh'>
+            <div className='grid-hor'>
+                <div style={{ textAlign: 'left' }}>
+                    <h1 className='text-primary' style={{ fontSize: '3.5rem', font: 'Candal', fontWeight: 'bolder' }}>Event.io</h1>
+                    <h1 style={{ color: 'darkgray' }}>Saving you time when you need it most</h1>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+            <div className='grid-hor'>
+                <div style={{ maxWidth: '20vw', textAlign: 'center' }}>
+                  <img src={radar} style={{ maxWidth: '70%' }} alt="Logo"/>
+                </div>
+            
+                <div style={{ verticalAlign: 'center' }}>
+                  <div style={{ textAlign: 'right' }}>
+                    <form onSubmit={onSubmit}>
+                        <div>
+                        <label
+                            htmlFor='email'
+                            className='login-label'
+                        >
+                            Email
+                        </label>
+                        <input
+                            type='email'
+                            name='email'
+                            value={email}
+                            onChange={onChange}
+                            required
+                            style={{ marginLeft: 'auto', width: '14vw' }}
+                        />
+                        </div>
+                        <div>
+                        <label
+                            htmlFor='password'
+                            className='login-label'
+                        >
+                            Password
+                        </label>
+                        <input
+                            type='password'
+                            name='password'
+                            value={password}
+                            onChange={onChange}
+                            required
+                            style={{ marginLeft: 'auto', width: '14vw' }}
+                        />
+                        </div>
+                        <div>
+                        {/* eslint-disable-next-line */}
+                        <div className='grid-hor'>
+                            <Link to='/register'>
+                                <input
+                                type='submit'
+                                value='Register'
+                                className='btn btn-block'
+                                style={{ marginLeft: 'auto', width: '5vw', backgroundColor: 'lightgray' }}
+                                />
+                            </Link>
+                            <a href='#'>
+                                <input
+                                type='submit'
+                                value='Login'
+                                className='btn btn-primary btn-block'
+                                style={{ marginLeft: 'auto', width: '5vw' }}
+                                />
+                            </a>
+                        </div>
+                        </div>
+                    </form>
+                </div>
+              </div>
+            </div>
           </div>
-          <div style={{ margin: '0 auto' }}>
-            <form onSubmit={onSubmit}>
-                <div>
-                <label
-                    htmlFor='email'
-                    className='login-label'
-                >
-                    Email:
-                </label>
-                <input
-                    type='email'
-                    name='email'
-                    value={email}
-                    onChange={onChange}
-                    required
-                    style={{ marginLeft: 'auto' }}
-                />
-                </div>
-                <div>
-                <label
-                    htmlFor='password'
-                    className='login-label'
-                >
-                    Password:{' '}
-                </label>
-                <input
-                    type='password'
-                    name='password'
-                    value={password}
-                    onChange={onChange}
-                    required
-                    style={{ marginLeft: 'auto' }}
-                />
-                </div>
-                <div>
-                {/* eslint-disable-next-line */}
-                <a href='#'>
-                    <input
-                    type='submit'
-                    value='Login'
-                    className='btn btn-primary btn-block'
-                    style={{ marginLeft: '16.5rem' }}
-                    />
-                </a>
-                </div>
-            </form>
-          </div>
-        </div>
       );
 };
 
