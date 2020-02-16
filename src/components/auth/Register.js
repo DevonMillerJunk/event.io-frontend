a2359e8d8fa01644b8b099832504c7b87db4742f
Cameron <orrca14@gmail.com>
2020-02-15 20:17:19 -0500

    Finished the interface for the registration page

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import radar from './radar1.png';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [question, setQuestion] = useState({
     email: '',
     name: '',
-    username: '',
     password: '',
     confirmPassword: ''
   });
 
-  const { email, name, username, password, confirmPassword } = question;
+  const { email, name, password, confirmPassword } = question;
 
   const onChange = e =>
     setQuestion({ ...question, [e.target.name]: e.target.value });
@@ -27,118 +28,116 @@ const Register = () => {
       confirmPassword === ''
     ) {
       console.log('Please fill out all fields.');
-    }// } else if (username) {
-    //   register({
-    //     email,
-    //     name,
-    //     username,
-    //     password
-    //   });
-    // } else {
-    //   register({
-    //     email,
-    //     name,
-    //     password
-    //   });
-    // }
+    }else {
+      console.log(email, name, password);
+    }
   };
 
   // if (!isAuthenticated) {
     return (
-      <div className='card card-lg'>
-        <h2 style={{ fontSize: '2rem' }}>Welcome to</h2>
-        <h1 className='mac-header-nbm'>Event</h1>
-        <h1 className='mac-header-nbm'>.io</h1>
-        <form onSubmit={onSubmit}>
-          <label
-            htmlFor='email'
-            className='login-label'
-            style={{ marginLeft: '16.5rem' }}
-          >
-            Email:
-          </label>
-          <input
-            type='email'
-            name='email'
-            value={email}
-            placeholder='john@gmail.com'
-            onChange={onChange}
-            required
-            style={{ marginLeft: '16.5rem' }}
-          />
-          <label
-            htmlFor='name'
-            className='login-label'
-            style={{ marginLeft: '16.5rem' }}
-          >
-            Name:
-          </label>
-          <input
-            type='text'
-            name='name'
-            value={name}
-            placeholder='Johnny Appleseed'
-            onChange={onChange}
-            required
-            style={{ marginLeft: '16.5rem' }}
-          />
-          <label
-            htmlFor='username'
-            className='login-label'
-            style={{ marginLeft: '16.5rem' }}
-          >
-            Username:
-          </label>
-          <input
-            type='text'
-            name='username'
-            value={username}
-            placeholder='(Optional - Will just be set to your name if none entered.)'
-            onChange={onChange}
-            style={{ marginLeft: '16.5rem' }}
-          />
-          <label
-            htmlFor='email'
-            className='login-label'
-            style={{ marginLeft: '16.5rem' }}
-          >
-            Password:
-          </label>
-          <input
-            type='password'
-            name='password'
-            value={password}
-            onChange={onChange}
-            required
-            style={{ marginLeft: '16.5rem' }}
-          />
-          <label
-            htmlFor='email'
-            className='login-label'
-            style={{ marginLeft: '16.5rem' }}
-          >
-            Confirm Password:
-          </label>
-          <input
-            type='password'
-            name='confirmPassword'
-            value={confirmPassword}
-            onChange={onChange}
-            required
-            style={{ marginLeft: '16.5rem' }}
-          />
-          <input
-            type='submit'
-            value='Register'
-            className='btn btn-primary btn-block'
-            style={{ marginLeft: '16.5rem' }}
-          />
-        </form>
-      </div>
-    );
-    //   } else {
-//     return <Redirect to='/' />;
-//   }
+        <div className='card-norm card-nh card-lg'>
+            <div className='grid-hor'>
+                <div style={{ textAlign: 'left' }}>
+                    <h1 className='font-effect-shadow-multiple' style={{ fontSize: '4.5rem', fontWeight: 'bolder', color: '#032275' }}>Event.io</h1>
+                </div>
+                <div style={{ textAlign: 'right' }}>
+                    <label
+                        htmlFor='email'
+                        className='login-label'
+                        style={{ marginLeft: 'auto', textAlign: 'right' }}
+                    >
+                        Email:
+                    </label>
+                    <input
+                        type='email'
+                        name='email'
+                        value={email}
+                        placeholder='john@gmail.com'
+                        onChange={onChange}
+                        required
+                        style={{ marginLeft: 'auto', textAlign: 'right' }}
+                    />
+                </div>
+            </div>
+            <div className='grid-hor'>
+                <div style={{ maxWidth: '20vw', textAlign: 'center' }}>
+                  <img src={radar} style={{ maxWidth: '70%' }} alt="Logo"/>
+                </div>
+            
+                <div style={{ verticalAlign: 'center' }}>
+                  <div style={{ textAlign: 'right' }}>
+                  <form onSubmit={onSubmit}>
+                    
+                    <label
+                        htmlFor='name'
+                        className='login-label'
+                        style={{ marginLeft: 'auto' }}
+                    >
+                        Name:
+                    </label>
+                    <input
+                        type='text'
+                        name='name'
+                        value={name}
+                        placeholder='Johnny Appleseed'
+                        onChange={onChange}
+                        required
+                        style={{ marginLeft: 'auto', textAlign: 'right' }}
+                    />
+                    <label
+                        htmlFor='email'
+                        className='login-label'
+                        style={{ marginLeft: 'auto' }}
+                    >
+                        Password:
+                    </label>
+                    <input
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={onChange}
+                        required
+                        style={{ marginLeft: 'auto' }}
+                    />
+                    <label
+                        htmlFor='email'
+                        className='login-label'
+                        style={{ marginLeft: 'auto' }}
+                    >
+                        Confirm Password:
+                    </label>
+                    <input
+                        type='password'
+                        name='confirmPassword'
+                        value={confirmPassword}
+                        onChange={onChange}
+                        required
+                        style={{ marginLeft: 'auto' }}
+                    />
+                    <div className='grid-hor'>
+                            <Link to='/login'>
+                                <input
+                                type='submit'
+                                value='Login'
+                                className='btn btn-block btn-primary'
+                                style={{ marginLeft: 'auto', width: '5vw', backgroundColor: 'lightgray', color: 'black' }}
+                                />
+                            </Link>
+                            <a href='#'>
+                                <input
+                                type='submit'
+                                value='Register'
+                                className='btn btn-block btn-primary'
+                                style={{ marginLeft: 'auto', width: '5vw' }}
+                                />
+                            </a>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+      );
 };
 
 export default Register;
